ef1ac0a65641ef3b9940423e70a64f685ec2bb33
Cesar Soares Lucas <cslucas@openjdk.org>
2025-06-19 18:02:41 +0000

    8359064: Expose reason for marking nmethod non-entrant to JVMCI client
    Reviewed-by: dnsimon, never
    (cherry picked from commit c73ad280f333145a923027dc082cd796a93b3a94)

--- src/jdk.internal.vm.ci/share/classes/jdk/vm/ci/hotspot/HotSpotNmethod.java
+++ src/jdk.internal.vm.ci/share/classes/jdk/vm/ci/hotspot/HotSpotNmethod.java
@@ -151,7 +151,7 @@ public class HotSpotNmethod extends HotSpotInstalledCode {
      * Invalidate this nmethod using the reason specified in {@code invalidationReason} and
      * optionally deoptimize the method if {@code deoptimize} is set.
      *
-     * @param deoptimize         whether or not to deoptimize the method.
+     * @param deoptimize whether or not to deoptimize the method.
      * @param invalidationReason invalidation reason code.
      */
     public void invalidate(boolean deoptimize, int invalidationReason) {
